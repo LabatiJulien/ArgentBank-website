10b761aa51e67e1a4b62ac7beb6d84baa6de8857
LabatiJulien <julien.labati@hotmail.fr>
2024-04-24 14:55:38 +0200

    use browser router et installation dépendance

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import Home from './pages/Home';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

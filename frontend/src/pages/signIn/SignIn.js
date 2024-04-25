9fa28c602dcffa53fdabf7dc57db3186da59cb4c
LabatiJulien <julien.labati@hotmail.fr>
2024-04-25 09:58:29 +0200

    Paramétrage de SignIn pour communication API

--- frontend/src/pages/signIn/SignIn.js
+++ frontend/src/pages/signIn/SignIn.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SignIn.css'; 
 import Navbar from '../../components/navbar/Navbar';
 import Footer from '../../components/footer/Footer';
+import axios from 'axios';
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      try {
+        const response = await axios.post('http://localhost:3001/api/v1/user/login', formData);
+        console.log(response.data); 
+        
+        window.location.href = '/profile';
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
+  const validateForm = () => {
+    let errors = {};
+    let isValid = true;
+
+    if (!formData.email.trim()) {
+      errors.email = 'Email is required';
+      isValid = false;
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = 'Email is invalid';
+      isValid = false;
+    }
+
+    if (!formData.password.trim()) {
+      errors.password = 'Password is required';
+      isValid = false;
+    }
+
+    setErrors(errors);
+    return isValid;
+  };
+
   return (
     <> 
     <Navbar />
@@ -11,14 +58,16 @@ const SignIn = () => {
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        {errors.email && <p className="error-message">{errors.email}</p>}
+        {errors.password && <p className="error-message">{errors.password}</p>}
+        <form onSubmit={handleSubmit}>
           <div className="input-wrapper">
-            <label htmlFor="username">Username</label>
-            <input type="text" id="username" />
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
           </div>
           <div className="input-wrapper">
             <label htmlFor="password">Password</label>
-            <input type="password" id="password" />
+            <input type="password" id="password" name="password" value={formData.password} onChange={handleChange} />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />

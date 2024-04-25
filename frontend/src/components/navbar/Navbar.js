111c94bec21e36c92a5aa4fa5d293e23b201a5d9
LabatiJulien <julien.labati@hotmail.fr>
2024-04-25 09:31:26 +0200

    Importation de Link pour signin

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,22 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; 
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="/">
+      <Link to="/" className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src="/argentBankLogo.png"
           alt="Argent Bank Logo"
         />
-       
-      </a>
+      </Link>
       <div>
-        <a className="main-nav-item" href="/sign-in">
+        <Link to="/login" className="main-nav-item">
           <i className="fa fa-user-circle"></i>
           Sign In
-        </a>
+        </Link>
       </div>
     </nav>
   );

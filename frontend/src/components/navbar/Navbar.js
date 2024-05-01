100fff9d40fb17c9f86d57771242b41e329a3cce
LabatiJulien <julien.labati@hotmail.fr>
2024-05-01 12:54:39 +0200

    récupération de user name requête Post

--- frontend/src/components/navbar/Navbar.js
+++ frontend/src/components/navbar/Navbar.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { logout } from '../../reducers';
 
 const Navbar = () => {
   const token = useSelector(state => state.auth.token);
+  const [userName, setUserName] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      try {
+        const response = await fetch('http://localhost:3001/api/v1/user/profile', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+          },
+        });
+        const data = await response.json();
+        if (response.ok) {
+          setUserName(data.body.firstName); 
+        } else {
+          console.error('Failed to fetch user profile:', data.message);
+        }
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      }
+    };
+
+    if (token) {
+      fetchUserProfile();
+    }
+  }, [token]);
 
   const handleLogout = () => {
     dispatch(logout());
@@ -24,10 +51,13 @@ const Navbar = () => {
       </Link>
       <div>
         {token ? (
-          <button onClick={handleLogout} className="main-nav-item">
-            <i className="fa fa-sign-out"></i>
-            Sign Out
-          </button>
+          <div>
+            <button onClick={handleLogout} className="main-nav-item">
+              <i className="fa fa-sign-out"></i>
+              Sign Out
+            </button>
+            <span className="main-nav-item">{userName}</span>
+          </div>
         ) : (
           <Link to="/login" className="main-nav-item">
             <i className="fa fa-user-circle"></i>
